3db4c68e6c568bfecb66815389996c0895394977
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-08-11 17:11:22 +0200

    [Tech] HealthBarActivator done

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/HealthBarActivator.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarActivator : MonoBehaviour
+{
+    public float Radius = 5f;
+    public LayerMask LayersToHit;
+    public RaycastHit infoSphere,infoRay;
+    // Update is called once per frame
+    void Update()
+    {
+        bool sphere, ray;
+        sphere = Physics.SphereCast(this.transform.position, Radius, transform.forward, out infoSphere, LayersToHit.value, 25);
+        ray = Physics.Raycast(this.transform.position, transform.forward, out infoRay, LayersToHit.value, 25);
+        if (!sphere && !ray)
+        {
+            return;
+        }
+       
+
+        HealthBarObject tryGetHBO= infoSphere.collider.GetComponentInChildren<HealthBarObject>();
+        if (tryGetHBO == null)
+        {
+            if(ray)
+            {
+                tryGetHBO = infoRay.collider.GetComponentInChildren<HealthBarObject>();
+                if (tryGetHBO != null)
+                {
+                    tryGetHBO.Show();
+                }
+                
+            }
+        }
+        else
+        {
+            tryGetHBO.Show();
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(infoSphere.point, Radius);
+    }
+}
