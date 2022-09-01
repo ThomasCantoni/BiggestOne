46eebcbc8bcc56e88a69c7099a68daef6722491c
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-09-01 12:41:55 +0200

    [tech] coin fix

--- ProgettoFinaleUnity_fixed/Assets/Scripts/DropObjDeath.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/DropObjDeath.cs
@@ -9,12 +9,13 @@ public class DropObjDeath : MonoBehaviour
     void Start()
     {
         HBE = GetComponent<HealthBarEnemy>();
+        HBE.OnEnemyDeath += Drop;
     }
     public void Drop()
     {
         for (int i = 0; i < objToDrops.Count; i++)
         {
-            Instantiate(objToDrops[i]);
+            Instantiate(objToDrops[i],this.transform.position,Quaternion.Euler(0,0,90));
         }
         Destroy(this);
     }
