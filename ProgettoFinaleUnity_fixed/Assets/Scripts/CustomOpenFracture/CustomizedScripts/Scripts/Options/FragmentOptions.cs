a85f26431b611a949b4edf2679fde61d87fec6bc
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-09-19 12:03:08 +0200

    [tech] fracture now customizable codewise

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/CustomOpenFracture/CustomizedScripts/Scripts/Options/FragmentOptions.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class FragmentOptions : MonoBehaviour
+{
+    public float DestroyTime = 3f;
+}
