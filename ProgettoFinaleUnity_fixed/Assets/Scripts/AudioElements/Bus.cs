a72b4ab72d23ad2717df929e968e0538110583f1
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-09-22 08:56:14 +0200

    [tech] added fmod project, package and a couple of sounds

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/AudioElements/Bus.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bus : MonoBehaviour
+{
+    FMOD.Studio.Bus bus;
+    [SerializeField][Range(-80f,10)]
+    private float Volume;
+    void Start()
+    {
+        bus = FMODUnity.RuntimeManager.GetBus("bus:/MusicBus");
+    }
+    public float DecibelToLinear(float dB)
+    {
+        float linear = Mathf.Pow(10f, dB / 20f);
+        return linear;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        bus.setVolume(DecibelToLinear(Volume));
+    }
+}
