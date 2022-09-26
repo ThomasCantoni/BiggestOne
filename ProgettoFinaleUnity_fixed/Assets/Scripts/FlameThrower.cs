2ff47c5ff9b90827f1583f9441046b29a8717d73
Deomoreo <deomoreo@gmail.com>
2022-09-26 12:33:51 +0200

    [Tech] Fix Flame Thrower

--- ProgettoFinaleUnity_fixed/Assets/Scripts/FlameThrower.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/FlameThrower.cs
@@ -6,16 +6,17 @@ public class FlameThrower : GenericGun
 {
     public ParticleSystem Ps;
     public float speed = 5f;
+    public bool isShooting;
     public override void Start()
     {
+        InputCooker.PlayerPressedShoot += () => Ps.Play(true);
+        InputCooker.PlayerReleasedShoot += () => Ps.Stop(false);
     }
 
 
     public override void Shoot()
     {
-        
-                Ps.gameObject.SetActive(true);
-
         DeductAmmo();
+
     }
 }
